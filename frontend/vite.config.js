bd0fbccb367d985a1c8b6e7b710af942772d5c61
kadappa savalagi <kadappasavalagi@gmail.com>
2025-03-09 18:11:22 +0530

    deploying

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: './frontend',  // If your Vite config and index.html are in the 'frontend' folder
+  build: {
+    outDir: 'dist',  // Change if your output directory is different
+  }
 })

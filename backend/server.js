a521b67c4394aeec08b2742c499df12052b356bd
kadappa savalagi <kadappasavalagi@gmail.com>
2025-03-09 17:54:21 +0530

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -58,6 +58,7 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(3000, () => {
-    console.log('Server running on http://localhost:3000');
+server.listen(process.env.PORT || 3000, () => {
+    console.log('Server running on port', process.env.PORT || 3000);
 });
+
